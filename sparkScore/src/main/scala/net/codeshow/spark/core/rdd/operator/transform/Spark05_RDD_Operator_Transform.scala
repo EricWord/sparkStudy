4213c6883b10319ac0c9103b9afdc18665d100d7
“cuigs” <cuiguangsong@miliantech.com>
2020-12-23 16:51:34 +0800

    glom

--- sparkScore/src/main/scala/net/codeshow/spark/core/rdd/operator/transform/Spark05_RDD_Operator_Transform.scala
+++ sparkScore/src/main/scala/net/codeshow/spark/core/rdd/operator/transform/Spark05_RDD_Operator_Transform.scala
@@ -2,20 +2,16 @@ package net.codeshow.spark.core.rdd.operator.transform
 
 import org.apache.spark.{SparkConf, SparkContext}
 
-object Spark04_RDD_Operator_Transform2 {
+object Spark05_RDD_Operator_Transform {
   def main(args: Array[String]): Unit = {
     //@todo 准备环境
     val sparkConf = new SparkConf().setMaster("local[*]").setAppName("Operator")
     val sc = new SparkContext(sparkConf)
 
     //@todo 算子-map
-    val rdd = sc.makeRDD(List(List(1, 2), 3, List(4, 5)))
-
-    val flatRDD = rdd.flatMap {
-      case list: List[_] => list
-      case dat => List(dat)
-    }
-    flatRDD.collect().foreach(println)
+    val rdd = sc.makeRDD(List(1, 2, 3, 4), 2)
+    val glomRDD = rdd.glom()
+    glomRDD.collect().foreach(data => println(data.mkString(",")))
     sc.stop()
   }
 }
--- sparkScore/src/main/scala/net/codeshow/spark/core/rdd/operator/transform/Spark05_RDD_Operator_Transform_Test.scala
+++ sparkScore/src/main/scala/net/codeshow/spark/core/rdd/operator/transform/Spark05_RDD_Operator_Transform_Test.scala
@@ -2,20 +2,23 @@ package net.codeshow.spark.core.rdd.operator.transform
 
 import org.apache.spark.{SparkConf, SparkContext}
 
-object Spark04_RDD_Operator_Transform2 {
+object Spark05_RDD_Operator_Transform_Test {
   def main(args: Array[String]): Unit = {
     //@todo 准备环境
     val sparkConf = new SparkConf().setMaster("local[*]").setAppName("Operator")
     val sc = new SparkContext(sparkConf)
 
-    //@todo 算子-map
-    val rdd = sc.makeRDD(List(List(1, 2), 3, List(4, 5)))
+    //@todo 算子
+    val rdd = sc.makeRDD(List(1, 2, 3, 4), 2)
 
-    val flatRDD = rdd.flatMap {
-      case list: List[_] => list
-      case dat => List(dat)
-    }
-    flatRDD.collect().foreach(println)
+    val glomRDD = rdd.glom()
+
+    val maxRDD = glomRDD.map(
+      array => {
+        array.max
+      }
+    )
+    println(maxRDD.collect().sum)
     sc.stop()
   }
 }
