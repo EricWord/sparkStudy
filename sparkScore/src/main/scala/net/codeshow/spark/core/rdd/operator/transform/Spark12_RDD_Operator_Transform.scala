cee3662cda9e9f1777ff4bd0ae559b74fad369b7
“cuigs” <cuiguangsong@miliantech.com>
2020-12-24 11:06:21 +0800

    sortBy

---
+++ sparkScore/src/main/scala/net/codeshow/spark/core/rdd/operator/transform/Spark12_RDD_Operator_Transform.scala
@@ -0,0 +1,18 @@
+package net.codeshow.spark.core.rdd.operator.transform
+
+import org.apache.spark.{SparkConf, SparkContext}
+
+object Spark12_RDD_Operator_Transform {
+  def main(args: Array[String]): Unit = {
+    //@todo 准备环境
+    val sparkConf = new SparkConf().setMaster("local[*]").setAppName("Operator")
+    val sc = new SparkContext(sparkConf)
+    //@todo 算子
+    val rdd = sc.makeRDD(List(6, 2, 4, 5, 3, 1), 2)
+    val newRDD = rdd.sortBy(num => num)
+    newRDD.saveAsTextFile("output")
+
+
+    sc.stop()
+  }
+}
---
+++ sparkScore/src/main/scala/net/codeshow/spark/core/rdd/operator/transform/Spark12_RDD_Operator_Transform1.scala
@@ -0,0 +1,21 @@
+package net.codeshow.spark.core.rdd.operator.transform
+
+import org.apache.spark.{SparkConf, SparkContext}
+
+object Spark12_RDD_Operator_Transform1 {
+  def main(args: Array[String]): Unit = {
+    //@todo 准备环境
+    val sparkConf = new SparkConf().setMaster("local[*]").setAppName("Operator")
+    val sc = new SparkContext(sparkConf)
+    //@todo 算子
+    val rdd = sc.makeRDD(List(("1", 1), ("11", 2), ("2", 3)), 2)
+    //sortBy方法可以根据指定的规则对数据源中的数据进行排序，默认为升序
+    //第二个参数可以改变排序的方式
+    //sortBy默认情况下不会改变分区，但是中间存在shuffle操作
+    val newRDD = rdd.sortBy(t => t._1.toInt,ascending = false)
+    newRDD.collect().foreach(println)
+
+
+    sc.stop()
+  }
+}
