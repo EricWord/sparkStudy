5e625172bd2a8df29ee25883edb399b1468968b4
“cuigs” <cuiguangsong@miliantech.com>
2020-12-24 14:12:59 +0800

    reduceByKey

---
+++ sparkScore/src/main/scala/net/codeshow/spark/core/rdd/operator/transform/Spark15_RDD_Operator_Transform.scala
@@ -0,0 +1,24 @@
+package net.codeshow.spark.core.rdd.operator.transform
+
+import org.apache.spark.{HashPartitioner, SparkConf, SparkContext}
+
+object Spark15_RDD_Operator_Transform {
+  def main(args: Array[String]): Unit = {
+    //@todo 准备环境
+    val sparkConf = new SparkConf().setMaster("local[*]").setAppName("Operator")
+    val sc = new SparkContext(sparkConf)
+    //@todo 算子
+    val rdd = sc.makeRDD(List(
+      ("a", 1), ("a", 2), ("a", 3), ("b", 4)
+    ))
+
+    //reduceByKey:相同的key的数据进行value数据的聚合操作
+    //scala语言中一般的聚合操作都是两两聚合，spark基于scala开发,所以spark的聚合也是两两聚合
+    //如果key的数据只有1个，是不会参与运算的
+    val reduceRDD = rdd.reduceByKey(_ + _)
+    reduceRDD.collect().foreach(println)
+
+
+    sc.stop()
+  }
+}
