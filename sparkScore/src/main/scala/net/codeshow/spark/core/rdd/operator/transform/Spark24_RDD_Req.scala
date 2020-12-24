607e34f28cb69862a7722a5d119944a0ed94f601
“cuigs” <cuiguangsong@miliantech.com>
2020-12-24 18:09:50 +0800

    案例实操

--- sparkScore/src/main/scala/net/codeshow/spark/core/rdd/operator/transform/Spark24_RDD_Req.scala
+++ sparkScore/src/main/scala/net/codeshow/spark/core/rdd/operator/transform/Spark24_RDD_Req.scala
@@ -4,19 +4,48 @@ import org.apache.spark.{SparkConf, SparkContext}
 
 object Spark24_RDD_Req {
   def main(args: Array[String]): Unit = {
-    //@todo 准备环境
     val sparkConf = new SparkConf().setMaster("local[*]").setAppName("Operator")
     val sc = new SparkContext(sparkConf)
-    //@todo 算子
-    val rdd1 = sc.makeRDD(List(
-      ("a", 1), ("b", 2) //, ("c", 3),
-    ))
-    val rdd2 = sc.makeRDD(List(
-      ("a", 4), ("b", 5), ("c", 6),
-    ))
-    //cogroup: connect + group
-    val cgRDD = rdd1.cogroup(rdd2)
-    cgRDD.collect().foreach(println)
+    //@todo 案例实操
+    //1.获取原始数据:时间戳、省份、城市、用户
+    val dataRDD = sc.textFile("datas/agent.log")
+
+    //2.将原始数据进行结构的转换，方便统计
+    //((省份,广告),1)
+
+    val mapRDD = dataRDD.map(
+      line => {
+        val datas = line.split(" ")
+        ((datas(1), datas(4)), 1)
+      }
+    )
+
+    //3.将转换结构后的数据，进行分组聚合
+    // ((省份,广告),1) => ((省份,广告),sum)
+    val reduceRDD = mapRDD.reduceByKey(_ + _)
+    //4.将聚合的结果进行结构的转换
+    // ((省份,广告),sum) => (省份(广告,sum))
+    val newMapRDD = reduceRDD.map {
+      case ((prv, ad), sum) => {
+        (prv, (ad, sum))
+      }
+    }
+
+    //5.将转换结构后的数据根据省份进行分组
+    //(省份，[(广告A,sumA),(广告B,sumB)])
+
+    val groupRDD = newMapRDD.groupByKey()
+    //6.将分组后的数据组内排序(降序),取前3名
+    val resultRDD = groupRDD.mapValues(
+      iter => {
+        iter.toList.sortBy(_._2)(Ordering.Int.reverse)
+      }
+    )
+
+    //7.采集数据，打印到控制台
+    resultRDD.collect().foreach(println)
+
+
     sc.stop()
   }
 }
