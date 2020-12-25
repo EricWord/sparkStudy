f6df06e152ad4bcd1deb8c1edc21b925ea3da804
“cuigs” <cuiguangsong@miliantech.com>
2020-12-25 17:38:41 +0800

    RDD持久化

---
+++ sparkScore/src/main/scala/net/codeshow/spark/core/rdd/persist/Spark04_RDD_Persist.scala
@@ -0,0 +1,34 @@
+package net.codeshow.spark.core.rdd.persist
+
+import org.apache.spark.storage.StorageLevel
+import org.apache.spark.{SparkConf, SparkContext}
+
+object Spark04_RDD_Persist {
+  def main(args: Array[String]): Unit = {
+    val sparkConf = new SparkConf().setMaster("local[*]").setAppName("Persist")
+    val sc = new SparkContext(sparkConf)
+
+    val list = List("Hello Scala", "Hello Spark")
+
+    val rdd = sc.makeRDD(list)
+    val flatRDD = rdd.flatMap(_.split(" "))
+    val mapRDD = flatRDD.map(word => {
+      println("@@@@@@@@@@@@@@@")
+      (word, 1)
+    }
+
+    )
+
+    //cache默认持久化的操作，只能将数据保存到内存中
+    //如果想要保存到磁盘文件，需要更改存储级别
+    //    mapRDD.cache()
+    mapRDD.persist(StorageLevel.DISK_ONLY)
+    val reduceRDD = mapRDD.reduceByKey(_ + _)
+    reduceRDD.collect().foreach(println)
+    println("****************")
+    val groupRDD = mapRDD.groupByKey()
+    groupRDD.collect().foreach(println)
+    sc.stop()
+  }
+
+}
